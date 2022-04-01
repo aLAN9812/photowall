3c25823db7a1b350d94fc488ef780c494a6901b5
aLAN9812 <chantszlun1218@gmail.com>
2022-04-01 16:46:07 -0400

    Setup basic layout

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import ReactDom from 'react-dom'
 import Main from './components/main'
+import './styles/stylesheet.css'
 
 ReactDom.render(<Main/>, document.getElementById('root'))
