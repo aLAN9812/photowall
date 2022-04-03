7466fea479543b4e4d131ee268125668ca885af0
aLAN9812 <chantszlun1218@gmail.com>
2022-04-03 18:11:48 -0400

    Finished add post page layout and modified styles

--- src/components/AddPhoto.js
+++ src/components/AddPhoto.js
@@ -3,7 +3,19 @@ import PropTypes from 'prop-types'
 
 class AddPhoto extends Component {
     render() {
-        return <h1>Add Post</h1>
+        return (
+            <div>
+                <h1>Add Post</h1>
+                <div className='form'>
+                    <form>
+                        <input type='text' placeholder='Link'/>
+                        <input type='text' placeholder='Description'/>
+                        <button>Post</button>
+                    </form>
+                </div>
+                
+            </div>
+        )
     }
 }
 
