2c029f56a5e138e6837010858af4a08960115d37
aLAN9812 <chantszlun1218@gmail.com>
2022-04-03 18:48:14 -0400

    Added sort function to make the order posts by date

--- src/components/Photowall.js
+++ src/components/Photowall.js
@@ -7,7 +7,11 @@ function Photowall(props) {
     return <div>
                 <Link className='add-icon' to='/AddPhoto'></Link>
                 <div className='photoGrid'>
-                    {props.posts.map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
+                    {props.posts
+                        .sort(function(x, y) {
+                            return y.id - x.id
+                        })
+                        .map((post, index) => <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto}/>)}
                 </div>
            </div>
 }
